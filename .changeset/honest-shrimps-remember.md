fcdbef8c8e48fd93eeaf0d99a400b30a82909b28
Chris Neven <chrisn@oberon.nl>
2022-06-09 21:50:21 +0200

    fix: add file extension to useRouter import path

---
+++ .changeset/honest-shrimps-remember.md
@@ -0,0 +1,5 @@
+---
+"next-router-event": patch
+---
+
+Add file extension to useRouter import path
--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/router.js";
 
 export type Event =
   | "routeChangeStart"
